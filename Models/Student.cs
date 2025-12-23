708e9fef548fec8f4e67b93547d04c70b083961a
Mark Christian Mallari <mallariftw01@gmail.com>
2025-12-24 00:00:45 +0800

    feat: Add Student Repo and Models

---
+++ Models/Student.cs
@@ -0,0 +1,42 @@
+﻿namespace FirstWinForm.Models
+{
+
+
+    public class Student
+    {
+        private int studentId;
+        private string firstName, lastName;
+        private int age;
+        private string course;
+
+        public int StudentId
+        {
+            get => studentId;
+            set => studentId = value;
+        }
+
+        public string FirstName
+        {
+            get => firstName;
+            set => firstName = value;
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            set => lastName = value;
+        }
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+
+        public string Course
+        {
+            get => course;
+            set => course = value;
+        }
+    }
+}
---
+++ Repositories/StudentRepository.cs
@@ -0,0 +1,62 @@
+﻿
+using FirstWinForm.Database;
+using FirstWinForm.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+namespace FirstWinForm.Repositories
+{
+    public class StudentRepository
+    {
+        public List<Student> GetAll()
+        {
+            List<Student> students = new List<Student>();
+
+            string query = "SElECT * FROM students";
+
+            using (var conn = DatabaseHelper.GetConnection())
+            using (var cmd = new SqlCommand(query, conn))
+            {
+                conn.Open();
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        students.Add(new Student
+                        {
+                            StudentId = reader.GetInt32(0),
+                            FirstName = reader.GetString(1),
+                            LastName = reader.GetString(2),
+                            Age = reader.GetInt32(3),
+                            Course = reader.GetString(4),
+                        });
+
+
+                    }
+                }
+
+                return students;
+
+            }
+        }
+
+        public bool Add(Student student)
+        {
+            string query = @"INSERT INTO students(first_name,last_name,age,course) VALUES (@firstName,@lastName,@age,@course)";
+
+            using (var conn = DatabaseHelper.GetConnection())
+            using (var cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.Add("@firstName", SqlDbType.NVarChar).Value = student.FirstName;
+                cmd.Parameters.Add("@lastName", SqlDbType.NVarChar).Value = student.LastName;
+                cmd.Parameters.Add("@age", SqlDbType.NVarChar).Value = student.Age;
+                cmd.Parameters.Add("@course", SqlDbType.NVarChar).Value = student.Course;
+
+                conn.Open();
+
+                return cmd.ExecuteNonQuery() > 0;
+
+            }
+        }
+    }
+}
